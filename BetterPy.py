1d10398456b1863caa9c92e71d67cc8b810d565b
OdorajBotoj <odoraj_botoj@tom.com>
2020-12-30 10:39:29 +0800

    BetterPy:V1.0.1.20201230.修复了一点BUG

--- BetterPy.py
+++ BetterPy.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8 -*-
 from time import time
-__version__ = '1.0.0.20201229'
+__version__ = '1.0.1.20201230'
 __another__ = 'OdorajBotoj'
 def betterpyInfo():
     print('BetterPy:',__version__,'By:',__another__)
@@ -39,5 +39,16 @@ def _CASE(i,d):
                 out = eval(d[a])
                 break
             else:pass
-    else:raise KeyError('ERROR:{} is not in {}'.format(i,d))
+    else:raise KeyError('ERROR:{} is not in {}'.format(i,d));out='ERR'
     return out
+
+def _HELP(name=''):
+    d=dict({'betterpyInfo':'betterpyInfo()\n打印程序信息','_BK':'_BK(<提示信息>)\n设置程序中断','_DEBUG':'_DEBUG(<一个或多个变量>)\n输出变量信息并中断','_QUIT':'_QUIT(<提示信息>)\n中断并退出','_PLUS':'_PLUS(<变量a>,<变量b>)\n高精度加法','_RUN_CLOCK':'_RUN_CLOCK(<代码>)\n执行代码并计算运行所耗时间','_CASE':'_CASE(<变量>,<目标字典>)\n当变量等于字典中的某个键时,执行该键对应的值.\n例:_CASE(1,{1:\'print(\'1\')\',2:\'print(\'2\')\'})的输出为:1.\n当变量不在目标字典的范围内时，会引发KeyError','_HELP':'_HELP(<函数名>)\n输出该函数对应的帮助.当函数名不在模块的范围内时，会引发KeyError'})
+    if name:
+        if name in d.keys():
+            print(d[name])
+        else:print('ERR');raise KeyError('ERROR:There is not a function named {} in this module.'.format(name))
+    else:
+        for i in d.keys():
+            print(d[i])
+    return
